c7211f2a8492333c85ec0ff5bd261732d50c8381
Shafa007 <shafayet.aoungon@gmail.com>
2021-06-29 21:17:03 +0600

    view

---
+++ HomePage.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>AIUB Homepage</title>
+</head>
+
+<body style="background-color:#D5D8DC">
+
+  <h3><p align="right">
+<a  href="../controller/loginn.php">Login</a><br>
+<a href="applynow.php">Apply Now</a><br>
+<a href="officialprogram.php">Programs on process...</a><br>
+<a href="../controller/helpandsupport.php">Help & Support</a><br>
+<a href="ContactUs.php">Contact Us</a><br></h3></p>
+
+
+<marquee direction="right" behavior="alternate" bgcolor="Black">
+<h1><a href="#" style="color:white";>American International University-Bangladesh</a></h1></marquee>
+<h1>
+
+  <a href="#"><marquee direction="left" behavior="alternate"  bgcolor="sky blue">
+-Where leaders are created</a></h1><br>
+  
+  
+<img src="logo.png" width="50" height="50" title="Logo of a company" alt="Logo of a company" /></marquee>
+
+<marquee marquee direction="left" behavior="alternate"  bgcolor="white">
+
+<img src="sk.jpg" width="450" height="350" title="Logo of a AIUB" alt="Logo of a AIUB" />
+<img src="fg.jpg" width="450" height="350" title="Logo of a AIUB" alt="Logo of a AIUB" />
+<img src="th.jpg" width="450" height="350" title="Logo of a AIUB" alt="Logo of a AIUB" />
+<img src="nn.jpg" width="450" height="350" title="Logo of a AIUB" alt="Logo of a AIUB" />
+<img src="ee.jpg" width="450" height="350" title="Logo of a AIUB" alt="Logo of a AIUB" />
+<img src="qq.jpg" width="450" height="350" title="Logo of a AIUB" alt="Logo of a AIUB" />
+
+</marquee>
+
+<br>
+
+
+<h6 align="left"><b>American International University-Bangladesh (AIUB)<br>
+ 408/1, Kuratoli, Khilkhet,<br>
+Dhaka 1229, Bangladesh<br>
+ info@aiub.edu <b></h6>
+
+  </body>
+  </html>
---
+++ applynow.php
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+<html>  
+<head>
+
+<title>Application</title>
+</head>
+<body style = "background-color: #F5CBA7 ">
+<h1><p align='center'>Please choose your desire program</p></h1>
+
+
+<h2><i><a href="../controller/undergraduate.php">Undergraduation</a></i></h2>
+
+<br><br>
+<br><br>
+
+<h2><i><a href="../controller/postgraduate.php">Postgraduation</a></i>
+</h2><br><br><br>
+
+<button><a href="HomePage.php">Go To HomePage</button>
+</body>
+</html>
